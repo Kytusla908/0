f47655c86531b68ba3d88430c31f84bf92fca6e9
Kytusla908 <88229203+Kytusla908@users.noreply.github.com>
2021-07-31 10:50:37 +0200

    Add files via upload

--- PY0101EN - Module 1.py
+++ PY0101EN - Module 1.py
@@ -1,3 +1,43 @@
 import sys
 print(sys)
 print(sys.version)
+
+print('Hello, Python') #This line prints a string
+print("Hello, Python")
+
+int(2.13423)
+type(int(2.13423))
+
+float('6.312')
+int(float('6.312'))
+type(int(float("6.312")))
+
+type(True)
+int(True)
+bool(1)
+int(False)
+bool(0)
+
+type(6/2)
+int(6/2)
+type(6//2)
+
+#How many hours has 160 minutes?
+160/60
+160//60
+
+#Variables
+x=3+2*2
+x
+y=(3+2)*2
+y
+z=x+y
+z
+
+
+
+
+
+
+
+
